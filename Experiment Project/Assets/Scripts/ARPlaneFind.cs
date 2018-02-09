c977b3b5cccbc952ce744c87c0f15375fb7abe8a
Neeraj Bedi <neerajbedico@gmail.com>
2018-02-09 17:39:24 +0530

    AR Plane Cube Move

---
+++ Experiment Project/Assets/Scripts/ARPlaneFind.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class ARPlaneFind : MonoBehaviour
+{
+    public GameObject TestCube;
+    private GameObject GetParentObject;
+    void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+       
+        RaycastHit hit = new RaycastHit();
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase.Equals(TouchPhase.Began))
+            {
+                
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.tag == "ArPlane")
+                    {
+                        Debug.Log("ArPlane ");
+                        GameObject clone = Instantiate(TestCube, hit.point, Quaternion.identity);
+                        GetParentObject = GameObject.FindGameObjectWithTag("TopCubeParent");
+                        clone.transform.parent = GetParentObject.transform;
+
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
---
+++ Experiment Project/Assets/Scripts/DestroyParentCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParentCube : MonoBehaviour
+{
+    public GameObject TopCube;
+
+	public void TopCubeDestroy()
+    {
+        Destroy(TopCube);
+	}
+
+    private void Update()
+    {
+        gameObject.transform.LookAt(Camera.main.transform.position);
+    }
+}
