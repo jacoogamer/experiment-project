dc8cd85619a0316edbba65accd78b16017a19fe9
Neeraj Bedi <neerajbedico@gmail.com>
2018-02-21 19:32:12 +0530

    Measurment Code Added

---
+++ Experiment Project/Assets/Scripts/ScrollItemManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScrollItemManager : MonoBehaviour
+{
+    public GameObject ScrollListTop;
+    public GameObject CloseButton, OpenButton;
+    public GameObject[] ArrayOfScrollButtons;
+    public GameObject CubePrefab;
+    public Material CubeColor;
+    public Animator ScrollViewTop;
+    private void Start()
+    {
+        CloseButton.SetActive(false);
+        OpenButton.SetActive(true);
+       
+
+        ScrollViewTop.SetBool("InScroll", false);
+
+    }
+    public void OpenMenu()
+    {
+        OpenButton.SetActive(false);
+        ScrollViewTop.SetBool("InScroll", true);
+        CloseButton.SetActive(true);
+        
+    }
+    public void CloseMenu()
+    {
+        ScrollViewTop.SetBool("InScroll", false);
+        OpenButton.SetActive(true);
+        CloseButton.SetActive(false);
+    }
+
+    public Vector3 MainCamOffset;
+    public void ClickSideMenuBtn(int BtnNum)
+    {
+         
+        foreach (GameObject obj in ArrayOfScrollButtons)
+        {
+            
+            obj.gameObject.GetComponent<Shadow>().enabled = false;
+            
+            
+        }
+        GameObject clone = Instantiate(CubePrefab, Camera.main.transform.position + MainCamOffset, Quaternion.identity);
+        ArrayOfScrollButtons[BtnNum].GetComponent<Shadow>().enabled = true;
+        CubeColor.color = ArrayOfScrollButtons[BtnNum].GetComponent<Image>().color;
+
+        GameObject GetParentObject = GameObject.FindGameObjectWithTag("TopCubeParent");
+        clone.transform.parent = GetParentObject.transform;
+    }
+
+   
+}
---
+++ Experiment Project/Assets/Scripts/SetGazeOnARPlane.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SetGazeOnARPlane : MonoBehaviour {
+
+    public GameObject ReticleTransform, FakeRecticleImage;
+    public Image ReticleImage;
+   
+    public Vector3 OrginalPosition;
+    public LayerMask mask;
+   
+    public Vector3 OrginalRotation;
+    
+   
+    // Use this for initialization
+    void Start () {
+       
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        SetPosition();
+        
+    }
+
+    public void SetPosition()
+    {
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward,out hitInfo,1000F, mask))
+        {
+            MeasureButton.SetActive(true);
+            ReticleTransform.transform.position = hitInfo.point;
+            ReticleTransform.transform.rotation = Quaternion.FromToRotation(Vector3.forward, hitInfo.normal);
+            FakeRecticleImage.SetActive(false);
+            ReticleTransform.SetActive(true);
+
+        }
+        else
+        {
+            FakeRecticleImage.SetActive(true);
+            ReticleTransform.SetActive(false);
+
+            
+                MeasureButton.SetActive(false);
+            
+
+
+        }
+    }
+
+    public Vector3 Measurements;
+    bool clickNum = true;
+    public GameObject PosFirst2,PosFirst;
+    public Text Measurement;
+    public GameObject MeasureButton;
+    public void CalculateSize()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 1000F, mask))
+        {
+           
+            if (clickNum == false)
+                {
+                    PosFirst2.transform.position = hitInfo.point;
+                    Vector3 rawSize = PosFirst.transform.position - PosFirst2.transform.position;
+                    Vector3 CovertData = new Vector3 (Mathf.Abs(rawSize.x * 10), Mathf.Abs(rawSize.y * 10), Mathf.Abs(rawSize.z * 10));
+                    
+                    Debug.Log(CovertData + "rawSize");
+                    clickNum = true;
+                    Measurement.text = CovertData.ToString();
+                } else if (clickNum == true)
+                {
+                    PosFirst.transform.position = hitInfo.point;
+                    clickNum = false;
+                }
+        }
+        
+
+            
+
+
+        
+
+    }
+}
