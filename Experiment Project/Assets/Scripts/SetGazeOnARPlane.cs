a9c9458d9d19c213f79f4e64fcea9cc3f2fe310f
Neeraj Bedi <neerajbedico@gmail.com>
2018-02-22 17:16:09 +0530

    Continues Measurement show text

--- Experiment Project/Assets/Scripts/SetGazeOnARPlane.cs
+++ Experiment Project/Assets/Scripts/SetGazeOnARPlane.cs
@@ -23,7 +23,7 @@ public class SetGazeOnARPlane : MonoBehaviour {
 	void Update ()
     {
         SetPosition();
-        
+        CalculateSize();
     }
 
     public void SetPosition()
@@ -54,35 +54,48 @@ public class SetGazeOnARPlane : MonoBehaviour {
     }
 
     public Vector3 Measurements;
-    bool clickNum = true;
+    int clickNum = 0;
     public GameObject PosFirst2,PosFirst;
-    public Text Measurement;
+    
     public GameObject MeasureButton;
     private GameObject clonels;
     public GameObject lrMeasument;
+
+    public void ClickOnMeasurmentButton()
+    {
+        if (clickNum == 0)
+            clickNum = 1;
+        else if (clickNum == 2)
+            clickNum = 0;
+    }
+
+
     public void CalculateSize()
     {
         RaycastHit hitInfo;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 1000F, mask))
         {
            
-            if (clickNum == false)
-                {
-                    PosFirst2.transform.position = hitInfo.point;
-                    Vector3 rawSize = PosFirst.transform.position - PosFirst2.transform.position;
-                    Vector3 CovertData = new Vector3 (Mathf.Abs(rawSize.x * 10), Mathf.Abs(rawSize.y * 10), Mathf.Abs(rawSize.z * 10));
-                clonels.SetActive(true);
-                clonels.GetComponent<LineRenderer>().SetPosition(1, hitInfo.point);
-                    Debug.Log(CovertData + "rawSize");
-                    clickNum = true;
-                    Measurement.text = CovertData.ToString();
-                } else if (clickNum == true)
+            if (clickNum == 1)
                 {
                     PosFirst.transform.position = hitInfo.point;
-                    clickNum = false;
+                    clickNum = 2;
                     clonels = Instantiate(lrMeasument, hitInfo.point,Quaternion.identity);
                 clonels.SetActive(false);
                     clonels.GetComponent<LineRenderer>().SetPosition(0, hitInfo.point);
+                }else if (clickNum == 2)
+                {
+                    PosFirst2.transform.position = hitInfo.point;
+                    Vector3 rawSize = PosFirst.transform.position - PosFirst2.transform.position;
+                    Vector3 CovertData = new Vector3(Mathf.Abs(rawSize.x * 10 * 2.54f), Mathf.Abs(rawSize.y * 10 * 2.54f), Mathf.Abs(rawSize.z * 10 * 2.54f));
+
+                    Debug.Log(CovertData + "rawSize");
+                clonels.GetComponentInChildren<TextMesh>().text = "(" +  CovertData.x.ToString("f1") + "cm Width," + CovertData.y.ToString("f1") + "cm Height,"+ CovertData.z.ToString("f1") + "cm Lenght)";
+                    clonels.GetComponentInChildren<TextMesh>().transform.localPosition = Vector3.zero;
+                
+                clonels.SetActive(true);
+                    clonels.GetComponent<LineRenderer>().SetPosition(1, hitInfo.point);
+                   
                 }
         }
         
